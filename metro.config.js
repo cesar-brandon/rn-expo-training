9c9379ba7a6ea3fa9b8d92ae8951a9f84da57137
cesar-brandon <cesarbrandon000@gmail.com>
2025-07-13 04:05:33 -0500

    Agrega soporte para activos wasm y configura encabezados COEP y COOP en el middleware del servidor para habilitar SharedArrayBuffer.

--- metro.config.js
+++ metro.config.js
@@ -13,6 +13,18 @@ config.resolver.alias = {
   '@': path.resolve(__dirname, 'src'),
 }
 
+// Add wasm asset support
+config.resolver.assetExts.push('wasm');
+
+// Add COEP and COOP headers to support SharedArrayBuffer
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    middleware(req, res, next);
+  };
+};
+
 // add nice web support with optimizing compiler + CSS extraction
 const { withTamagui } = require('@tamagui/metro-plugin')
 module.exports = withTamagui(config, {
